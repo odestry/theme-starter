94584356ab61ddee7cf44209c077473a64fe59b8
Youness Idbakkasse <itsblanklob@gmail.com>
2024-08-17 23:32:07 +0200

    added cleanup plugin

--- entry.theme.js
+++ entry.theme.js
@@ -1,3 +1,3 @@
 import './styles.css';
-
+console.log('hello world')
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import shopify from 'vite-plugin-shopify';
+import cleanup from '@by-association-only/vite-plugin-shopify-clean'
 
 export default defineConfig({
   plugins: [
@@ -7,6 +8,7 @@ export default defineConfig({
       additionalEntrypoints: ['entry.theme.js'],
       snippetFile: 'entry.liquid'
     }),
+    cleanup()
   ],
   build: {
     emptyOutDir: false
